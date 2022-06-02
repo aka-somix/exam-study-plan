ab2787361b2bd39545fe4b3b9acb20967f1109c7
akaSomix <somix.land@null.net>
2022-06-02 11:38:18 +0200

    server init

---
+++ server/src/index.js
@@ -0,0 +1,52 @@
+/*
+ * IMPORTS 
+ */
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+// locals
+const configs = require('./configs');
+const requestIdentifierMiddleware = require('./middleware/req-identifier')
+
+
+/*
+ * Build Express Server and add Middlewares
+ */
+const app = express();
+const port = configs.PORT;
+
+// Helmet for generic security
+app.use(helmet())
+
+// handle json payloads
+app.use(express.json());
+
+// TODO Add logging middleware
+
+// Add request identifier for logging purpose
+app.use(requestIdentifierMiddleware);
+
+// CORS 
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
+app.use(cors(corsOptions)); // For DEV purpose only.
+
+
+/*
+ * Define Routes
+ */
+app.get('/', (req, res, next) => {
+  console.log(`req identifier ${req.id}`);
+  res.statusCode = 200;
+  res.send({ test: "ok" });
+});
+
+
+/*
+ * Deploy Server
+ */
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${configs.PORT}`);
+});
---
+++ server/src/middleware/req-identifier/index.js
@@ -0,0 +1,11 @@
+/**
+ * TODO Insert Here the middleware to assign to each request an ID
+ */
+const uuid = require('uuid').v4
+
+const requestIdentifierMiddleware = (req, res, next) => {
+  req.id = uuid();
+  next();
+}
+
+module.exports = requestIdentifierMiddleware
